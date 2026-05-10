d02e5f3ac30a7beb1ba6cb39d40d4c6d136b1724
Devansh Gupta <shopykarttt@gmail.com>
2026-05-10 10:57:42 +0000

    Curated For You hatado

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,6 @@ import { CategoryList } from '@/components/home/CategoryList';
 import { ComboSection } from '@/components/home/ComboSection';
 import { OffersSection } from '@/components/home/OffersSection';
 import { PopularProducts } from '@/components/home/PopularProducts';
-import { PersonalizedOffers } from '@/components/home/PersonalizedOffers';
 
 export default function Home() {
   const [searchQuery, setSearchQuery] = useState('');
@@ -22,7 +21,6 @@ export default function Home() {
       {!searchQuery && (
         <div className="mt-6 space-y-6">
           <OfferSlider />
-          <PersonalizedOffers />
           <ComboSection />
           <OffersSection />
         </div>
