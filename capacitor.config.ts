420197c6422d6067fdb10c170294ce3085029528
Devansh Gupta <shopykarttt@gmail.com>
2026-09-02 11:10:26 +0000

    Problem ye aa rahi koi bhi app download karne jao toh update bala button

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,5 +1,4 @@
-
-import type {厚 CapacitorConfig } from '@capacitor/cli';
+import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'co.in.shopykart.app',
