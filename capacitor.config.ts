a02298ca77408fc284c137ffbd3064a2133cf04a
Devansh Gupta <shopykarttt@gmail.com>
2026-09-05 17:09:27 +0000

    final

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,18 +1,28 @@
-
-import type { CapacitorConfig } from '@capacitor/cli';
+import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'co.in.shopykart.app',
-  appName: 'ShopyKart',
-  webDir: 'out',
-  server: {
-    androidScheme: 'https',
-    cleartext: true,
-    allowNavigation: [
-      'shopykart.co.in',
-      '*.shopykart.co.in'
-    ]
-  }
-};
+    appName: 'ShopyKart',
+      webDir: 'out',
+        server: {
+            androidScheme: 'https',
+                cleartext: true,
+                    allowNavigation: [
+                          'shopykart.co.in',
+                                '*.shopykart.co.in'
+                                    ]
+                                      },
+                                        plugins: {
+                                            SplashScreen: {
+                                                  launchShowDuration: 3000,
+                                                        launchAutoHide: true,
+                                                              backgroundColor: "#ffffffff",
+                                                                    androidScaleType: "CENTER_CROP",
+                                                                          showSpinner: false,
+                                                                                splashFullScreen: true,
+                                                                                      splashImmersive: true
+                                                                                          }
+                                                                                            }
+                                                                                            };
 
-export default config;
+                                                                                            export default config;
