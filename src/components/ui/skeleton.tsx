0d394f9074a0e935da93cc8282e99045eb232447
Devansh Gupta <shopykarttt@gmail.com>
2026-09-20 06:33:54 +0000

    New

--- src/components/ui/skeleton.tsx
+++ src/components/ui/skeleton.tsx
@@ -12,7 +12,7 @@ function Skeleton({
   return (
     <div
       className={cn(
-        "relative overflow-hidden rounded-md bg-white border border-slate-50/50 before:absolute before:inset-0 before:-translate-x-full before:animate-[shimmer_2.5s_infinite] before:bg-gradient-to-r before:from-transparent before:via-slate-50/30 before:to-transparent",
+        "relative overflow-hidden rounded-md bg-white border border-slate-50/50 before:absolute before:inset-0 before:-translate-x-full before:animate-[shimmer_2.5s_infinite] before:bg-gradient-to-r before:from-transparent before:via-white/60 before:to-transparent",
         className
       )}
       {...props}
