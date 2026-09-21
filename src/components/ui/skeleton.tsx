19ffd76aacf22cb2d3cfd70edae8ff7022afc596
Devansh Gupta <shopykarttt@gmail.com>
2026-09-21 05:27:48 +0000

    Yeee

--- src/components/ui/skeleton.tsx
+++ src/components/ui/skeleton.tsx
@@ -13,7 +13,7 @@ function Skeleton({
   return (
     <div
       className={cn(
-        "relative overflow-hidden rounded-md bg-[#FFFAF5] before:absolute before:inset-0 before:-translate-x-full before:animate-shimmer before:bg-gradient-to-r before:from-transparent before:via-white/60 before:to-transparent",
+        "relative overflow-hidden rounded-md bg-[#FFFAF5] before:absolute before:inset-0 before:-translate-x-full before:animate-shimmer before:bg-gradient-to-r before:from-transparent before:via-white/80 before:to-transparent",
         className
       )}
       {...props}
